fa4a19774c204203a67749f4242804fe6c56decf
Joel Corey <joelcorey@fastmail.com>
2019-05-06 13:51:01 -0700

    remove comment

--- src/bus-routes/bus-routes-router.js
+++ src/bus-routes/bus-routes-router.js
@@ -9,6 +9,7 @@ routesRouter
   .get((req, res,next) => {
     RoutesService.getAllRoutes(req.app.get('db'))
       .then(stops => {
+        console.log(stops.map(RoutesService.serializeRoute))
         res.json(stops.map(RoutesService.serializeRoute))
       })
       .catch(next)
